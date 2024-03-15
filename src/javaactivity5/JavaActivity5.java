7a867c67b5cb669b23f4689a1ffb8c6e5d249cd9
REGIE <REGIE@LAPTOP-N9QJBTF3>
2024-03-15 14:26:09 +0800

    Engbino_Act5

---
+++ src/javaactivity5/JavaActivity5.java
@@ -0,0 +1,21 @@
+
+package javaactivity5;
+
+public class JavaActivity5 {
+
+
+    public static void main(String[] args) {
+    int day = 1;
+    switch (day) {
+      case 1:
+        System.out.println("Today is Saturday");
+        break;
+      case 2:
+        System.out.println("Today is Sunday");
+        break;
+      default:
+        System.out.println("Looking forward to the Weekdays");
+    }    
+  }
+    }
+    
